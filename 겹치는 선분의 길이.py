aec997a371aa3fb7ab4a3a4ba8af5c04c769e2be
dasol <qorwlsen@Gmail.com>
2023-03-08 18:38:35 +0900

    0308

---
+++ 겹치는 선분의 길이.py
@@ -0,0 +1,24 @@
+
+
+
+
+
+
+
+
+
+
+def solution(lines):
+    
+    sets_list = []
+    
+    for i in lines:
+        sets = set(range(min(i),max(i)))
+        sets_list.append(sets)
+                   
+            
+    return len(sets_list[0] & sets_list[1] | sets_list[0] & sets_list[2] | sets_list[1] & sets_list[2])
+    
+    
+a = solution([[0, 1], [2, 5], [3, 9]])
+print (a)
