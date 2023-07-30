cfca74122bf72c34cbb148b9b40e8015dc4e5663
dasol <qorwlsen@Gmail.com>
2023-07-31 00:53:21 +0900

    0731

--- 백준/동적프로그래밍/알고리즘수업_피보나치.py
+++ 백준/동적프로그래밍/알고리즘수업_피보나치.py
@@ -3,6 +3,19 @@ import sys
 
 def fin (n):
     if n ==1 or n ==2:
-        print(1)
+        return 1
     else:
         return fin(n-1) + fin(n-2)
+    
+def finbonacci(n):
+    stk = [0]* (n+1)
+    cou_fin = 0
+    stk[1],stk[2] = 1,1
+    for i in range(3, n+1):
+        stk[i] = stk[i-1]+stk[i-2]
+        cou_fin += 1
+    return cou_fin
+
+n = int(input())
+
+print(fin(n),  finbonacci(n))
