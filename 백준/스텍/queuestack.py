bf276afe2c8fbaa9f2e70122df659853bc3f5f72
dasol <qorwlsen@Gmail.com>
2023-08-07 01:37:16 +0900

    0807

---
+++ 백준/스텍/queuestack.py
@@ -0,0 +1,21 @@
+import sys
+from collections import deque
+
+input = sys.stdin.readline
+
+N = int(input())
+
+A = list(map(int, input().split()))
+B = list(map(int, input().split()))
+
+que = deque([val for idx, val in enumerate(B) if A[idx] == 0])
+print(que)
+M = int(input())
+
+C =  list(map(int, input().split()))
+
+
+for j in C:
+    que.appendleft(j)
+    print(que.pop(), end= " ")
+    
