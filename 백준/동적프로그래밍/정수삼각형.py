5eb3f2a5286db2b11e68eb89b2545d4ac615fa95
dasol <qorwlsen@Gmail.com>
2023-08-16 23:36:38 +0900

    0816

---
+++ 백준/동적프로그래밍/정수삼각형.py
@@ -0,0 +1,20 @@
+import sys
+input = sys.stdin.readline
+
+
+N = int(input())
+
+dp = [list(map(int,input().split())) for _ in range(N)]
+
+for i in range(1, N):
+    for j in range(0, i+1):
+        if j == 0:
+            dp[i][j] += dp[i-1][j]
+        elif j == i :
+            dp[i][j] += dp[i-1][j-1]
+            
+        else:
+            dp[i][j] += max(dp[i-1][j-1],dp[i-1][j])
+            
+print(dp) 
+print(max(dp[-1]))
