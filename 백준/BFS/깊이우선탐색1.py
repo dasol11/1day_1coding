fcd734575279b10e8b357e870dc22009d8fbe9ca
dasol <qorwlsen@Gmail.com>
2023-09-12 23:25:54 +0900

    0912

--- 백준/BFS/깊이우선탐색1.py
+++ 백준/BFS/깊이우선탐색1.py
@@ -1,8 +1,6 @@
 import sys
 input = sys.stdin.readline
-
-N, M, R = map(int,input().split())
-
+sys.setrecursionlimit(10 ** 7)
 
 def dfs(idx):
     global cnt
@@ -11,18 +9,22 @@ def dfs(idx):
         if visited[val] == 0:
             cnt += 1
             visited[val] = cnt
+            dfs(val)
         
+N, M, R = map(int,input().split())
 
-visited = [[0] for i in range(N+1)]
+visited = [0 for _ in range(N+1)]
 
-graph = [[] for _ in (M+1)]
+graph = [[] for _ in range(N+1)]
+
 cnt = 1
 for _ in range(M):
     a, b = map(int, input().split())
     graph[a].append(b)
     graph[b].append(a)
-    graph[a].sort()
-    
+for i in range(N+1):
+    graph[i].sort()
+
 dfs(R)
 
 for i in range(1, N+1):
