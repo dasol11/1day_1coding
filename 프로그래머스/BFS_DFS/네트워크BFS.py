88a371e4aebf6ed73108b684c2e96719af822db5
dasol <qorwlsen@Gmail.com>
2023-11-01 00:05:13 +0900

    ds

--- 프로그래머스/BFS_DFS/네트워크BFS.py
+++ 프로그래머스/BFS_DFS/네트워크BFS.py
@@ -1,11 +1,32 @@
+from collections import deque
 
-
-
-
-
-# def solution(n, computers):
-#     answer = 0
+def BFS(n, computers, com, visited):
+    visited[com] = True
+    que = deque()
+    que.append(com)
     
-#     for i in range(n):
-#         for j in 
-#     return answer
+    while len(que)> 0:
+        com = que.pop()
+        visited[com] = True
+        
+        for j in range(n):
+            if visited[j] == False and computers[j][com] == 1 and j != com:
+                que.append(j)    
+    
+            
+
+
+
+def solution(n, computers):
+    answer = 0
+    visited = [False] * n
+    
+    for com in range(n):
+        if visited[com] == False:
+            BFS(n, computers, com, visited)
+            answer += 1
+        
+    return answer
+
+
+print(solution(4, [[1, 1, 0, 0], [1, 1,0, 0], [0, 0, 1,0],[0,0,0,1]]))
